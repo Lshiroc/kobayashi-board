27a84bbc8d4714fa5de276b6f125c915d21f7e69
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-07-08 00:08:35 +0400

    preperations for adding sound

--- public/main.js
+++ public/main.js
@@ -11,6 +11,20 @@ const patterns = [
     ['<div class="w-[50px] h-[50px] sm:w-[90px] sm:h-[90px]"><a target="_blank" href="https://linkedin.com/in/zeynalmardanli" class="block rounded-full"><svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 17 17" height="100%" width="auto" xmlns="http://www.w3.org/2000/svg"><g></g><path d="M0.698 5.823h3.438v10.323h-3.438v-10.323zM2.438 0.854c-1.167 0-1.938 0.771-1.938 1.782 0 0.989 0.74 1.781 1.896 1.781h0.021c1.198 0 1.948-0.792 1.938-1.781-0.011-1.011-0.74-1.782-1.917-1.782zM12.552 5.583c-1.829 0-2.643 1.002-3.094 1.709v-1.469h-3.427c0 0 0.042 0.969 0 10.323h3.427v-5.761c0-0.312 0.032-0.615 0.114-0.843 0.251-0.615 0.812-1.25 1.762-1.25 1.238 0 1.738 0.948 1.738 2.333v5.521h3.428v-5.917c0-3.167-1.688-4.646-3.948-4.646z"></path></svg><a/></div>', '<div class="w-[50px] h-[50px] sm:w-[90px] sm:h-[90px]"><a target="_blank" href="https://github.com/Lshiroc" class="block rounded-full"><svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="100%" width="auto" xmlns="http://www.w3.org/2000/svg"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg><a/></div>', '<a href="https://lshiroc.com" target="_blank" class="w-12 h-12 sm:w-20 sm:h-20 border border-8 border-black bg-black rounded-md flex justify-center items-center"><div class="w-8 h-8 sm:w-14 sm:h-14 bg-[#fde93e] rounded-lg"></div></a>', '<div class="w-[50px] h-[50px] sm:w-[90px] sm:h-[90px]"><a target="_blank" href="mailto:zeynalmerdanli4321@gmail.com" class="block rounded-full"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="100%" width="auto" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"></path></svg></a></div>', '<div class="w-[45px] h-[45px] sm:w-[75px] sm:h-[75px]"><a target="_blank" href="https://x.com/lshiroc" class="block rounded-full"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="100%" width="auto" xmlns="http://www.w3.org/2000/svg"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"></path></svg></a>']
 ];
 
+function playSoundWithDelay(audio, delay) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        audio.play().catch(e => {
+          document.querySelector('#volumeBtn').addEventListener('click', () => {
+            audio.play();
+          }, { once: true });
+          document.querySelector('body').click();
+        });
+        resolve();
+      }, delay);
+    });
+}
+
 function getRandomPattern() {
     return patterns[Math.floor(Math.random() * patterns.length)];
 }
@@ -37,8 +51,15 @@ function displayPattern() {
     })
 
     reveals.forEach(reveal => {
-        reveal.style.animationName = "animate_reveal";  
+        reveal.style.animationName = "animate_reveal";
     })
+
+    const audio = new Audio('./pop.wav');
+    playSoundWithDelay(audio, 200);
+    playSoundWithDelay(audio, 700);
+    playSoundWithDelay(audio, 1200);
+    playSoundWithDelay(audio, 1700);
+    playSoundWithDelay(audio, 2200);
 
     document.querySelector('#myID').style.animationName = "showID";
 }
